8acc81473a3d8e36415edaee70855e272185803a
brendan o dowd <brenodowd@live.com>
2018-02-14 16:39:10 +0000

    added the buffer reader

--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -6,6 +6,32 @@ public Class TuneBook {
 
 	TuneBook(String s) {
 		
-		
-	}
+		BufferedReader inputStream = null;
+		try {
+			inputStream = new BufferedReader(new FileReader("abc.txt"));
+    
+			String l;
+			while ((l = inputStream.readLine()) != null) 
+			{
+				tunes.set(l);
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		} 
+		finally 
+		{
+			if (inputStream != null) {
+				try
+				{
+					inputStream.close();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		} 
+			
 }
